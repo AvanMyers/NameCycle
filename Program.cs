4ec35f98947b27a5b0ec22a40a56426ad5b3bbcb
AvanMyers <smail1341@gmail.com>
2024-12-04 11:23:59 +0300

    re

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Name_cycle_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string simbol;
+            string simbolName = "";
+
+            Console.WriteLine("Введите имя:");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите символ");
+            simbol = Console.ReadLine();
+
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                simbolName += simbol;
+            }
+
+            simbolName += simbol + simbol;
+
+            Console.WriteLine();
+            Console.WriteLine(simbolName);
+            Console.WriteLine(simbol + name + simbol);
+            Console.WriteLine(simbolName);
+
+            Console.ReadKey();
+        }
+    }
+}
